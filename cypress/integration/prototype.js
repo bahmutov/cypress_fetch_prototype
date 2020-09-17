1b4cd8631032905ae19ad91a29686d442c495cee
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2020-09-17 11:28:13 -0400

    show how to make this work

--- cypress/integration/prototype.js
+++ cypress/integration/prototype.js
@@ -1,6 +1,59 @@
 /// <reference types="Cypress" />
 
-it('Verify the prototype of fetch result is the same as Object.prototype', function() {
+// fails because the prototypes from two iframes are not the same
+it.skip('Verify the prototype of fetch result is the same as Object.prototype',
+  { experimentalFetchPolyfill: true }, function () {
   cy.visit('/')
+  cy.get('#result').should('have.text', 'true');
+})
+
+// this works - we can observe / spy on the network call
+// yet the prototype is correct
+it('try cy.route2',
+  { "experimentalNetworkStubbing": true }, function() {
+  cy.route2('/test')
+  cy.visit('/')
+  cy.get('#result').should('have.text', 'true');
+})
+
+// this works too
+it('try cy.route', function () {
+  cy.server()
+  cy.route('/test')
+  cy.visit('/')
+  cy.get('#result').should('have.text', 'true');
+})
+
+it('stub window.fetch', function() {
+  cy.visit('/', {
+    onBeforeLoad(w) {
+      cy.stub(w, 'fetch').resolves({
+        json() {
+          // make sure the object is constructed
+          // using the window under test's objects
+          return w.eval('({})')
+        }
+      })
+    }
+  })
+  cy.get('#result').should('have.text', 'true');
+})
+
+it('stub window.fetch with stringified object', function () {
+  const myData = {
+    foo: 'bar',
+    baz: 42
+  }
+  cy.visit('/', {
+    onBeforeLoad(w) {
+      cy.stub(w, 'fetch').resolves({
+        json() {
+          // make sure the object is constructed
+          // using the window under test's objects
+          return w.eval('(' + JSON.stringify(myData) + ')')
+        }
+      })
+    }
+  })
   cy.get('#result').should('have.text', 'true');
 })
--- public/index.js
+++ public/index.js
@@ -1,3 +1,7 @@
 fetch('/test')
   .then(response => response.json())
-  .then(data => document.getElementById("result").innerHTML = String(Object.getPrototypeOf(data) === Object.prototype));
+  .then(data => {
+    console.log('got data', data)
+    console.log('data prototype', Object.getPrototypeOf(data))
+    document.getElementById("result").innerHTML = String(Object.getPrototypeOf(data) === Object.prototype)
+  });
